1a96a4f1ffc937ae739ea388d5a1b6159754f4bd
Malte Swart <mswart@devtation.de>
2014-07-31 01:05:18 +0200

    ffi: extend memory pointer
    * Support passing block to initializer
    * Add attr reader for total size of underlying data
    * Support creating memory pointer without type (only size), needed for
      socket.rb

--- topaz/modules/ffi/memory_pointer.py
+++ topaz/modules/ffi/memory_pointer.py
@@ -19,14 +19,23 @@ class W_MemoryPointerObject(W_PointerObject):
     def singleton_method_allocate(self, space, args_w):
         return W_MemoryPointerObject(space)
 
-    @classdef.method('initialize', type_hint='symbol', size='int')
-    def method_initialize(self, space, type_hint, size=1):
-        w_FFI = space.find_const(space.w_kernel, 'FFI')
-        w_Type = space.find_const(w_FFI, 'Type')
-        self.w_type = space.find_const(w_Type, type_hint.upper())
-        sizeof_type = space.int_w(space.send(self.w_type, 'size'))
-        self.sizeof_memory = size * sizeof_type
+    @classdef.method('initialize', size='int')
+    def method_initialize(self, space, w_type_hint, size=1, block=None):
+        if space.is_kind_of(w_type_hint, space.w_symbol):
+            w_FFI = space.find_const(space.w_kernel, 'FFI')
+            w_Type = space.find_const(w_FFI, 'Type')
+            self.w_type = space.find_const(w_Type, space.symbol_w(w_type_hint).upper())
+            sizeof_type = space.int_w(space.send(self.w_type, 'size'))
+            self.sizeof_memory = size * sizeof_type
+        elif space.is_kind_of(w_type_hint, space.w_fixnum):
+            self.sizeof_memory = space.int_w(w_type_hint)
         memory = lltype.malloc(rffi.CArray(rffi.CHAR),
                                self.sizeof_memory,
                                flavor='raw', zero=True)
         self.ptr = rffi.cast(rffi.VOIDP, memory)
+        if block is not None:
+            return space.invoke_block(block, [self])
+
+    @classdef.method('total')
+    def method_total(self, space):
+        return space.newint(self.sizeof_memory)
